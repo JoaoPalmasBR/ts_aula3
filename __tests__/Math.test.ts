6898bacad1f6cf34ab1da1f7b175d48e6695fbb1
Joao Antonio <joaoantonios74@gmail.com>
2024-08-16 01:13:42 -0300

    adicionados testes de SUB, MULT

--- __tests__/Math.test.ts
+++ __tests__/Math.test.ts
@@ -22,6 +22,46 @@ describe('Testando Math', () => {
       it('Deve retornar a soma para um numero negativo e zero', () => {
           expect(math.sum(-1, 0)).toBe(-1);
       });
-
   });
+    describe('sub', () => {
+        it('Deve retornar a subtração para dois números positivos', () => {
+            expect(math.sub(1, 1)).toBe(0);
+        });
+        it('Deve retornar a subtração de um numero positivo com zero', () => {
+            expect(math.sub(1, 0)).toBe(1);
+        });
+        it('Deve retornar a subtração para dois zeros', () => {
+            expect(math.sub(0, 0)).toBe(0);
+        });
+        it('Deve retornar a subtração para um numero positivo e um negativo', () => {
+            expect(math.sub(1, -1)).toBe(2);
+        });
+        it('Deve retornar a subtração para dois números negativos', () => {
+            expect(math.sub(-1, -1)).toBe(0);
+        });
+        it('Deve retornar a subtração para um numero negativo e zero', () => {
+            expect(math.sub(-1, 0)).toBe(-1);
+        });
+    });
+    describe('mult', () => {
+        it('Deve retornar a multiplicação para dois números positivos', () => {
+            expect(math.mult(1, 1)).toBe(1);
+        });
+        it('Deve retornar a multiplicação de um numero positivo com zero', () => {
+            expect(math.mult(1, 0)).toBe(0);
+        });
+        it('Deve retornar a multiplicação para dois zeros', () => {
+            expect(math.mult(0, 0)).toBe(0);
+        });
+        it('Deve retornar a multiplicação para um numero positivo e um negativo', () => {
+            expect(math.mult(1, -1)).toBe(-1);
+        });
+        it('Deve retornar a multiplicação para dois números negativos', () => {
+            expect(math.mult(-1, -1)).toBe(1);
+        });
+        it('Deve retornar a multiplicação para um numero negativo e zero', () => {
+            expect(math.mult(-1, 0)).toBe(0);
+        });
+    });
+
 });
